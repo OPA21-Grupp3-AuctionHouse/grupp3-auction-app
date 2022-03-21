8dcb8629255218b71e352d1da36f1d5d40829872
Tajiana Odero <61429156+Taj-Ode@users.noreply.github.com>
2022-03-21 23:25:44 +0100

    Added some css for the welcomePage component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,12 @@
 
 const Login = () => {
-    return(<div className="Login">
-        <h2>Login</h2>
-        <input type="text" placeholder="Username"/>
-        <input type="text" placeholder="Password"/>
-    </div>)
-}
+    return(
+        <div className="login">
+            <h4>Login</h4>
+            <input type="text" placeholder="Username"/>
+            <input type="text" placeholder="Password"/>
+        </div>
+    );
+};
 
 export default Login;
