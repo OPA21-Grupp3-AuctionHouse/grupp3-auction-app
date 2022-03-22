be86a655b4d1e0cbcd8f24410a19ae866af6c7d5
mollerpw <89844600+mollerpw@users.noreply.github.com>
2022-03-22 14:19:25 +0100

    Css fixes

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -65,164 +65,166 @@ const Profile = () => {
 
   return (
     <>
-      <form>
-        <div class="col-auto">
-          <label for="staticEmail2" class="visually-hidden">
-            Username:
-          </label>
-          <input
-            type="text"
-            readonly
-            class="form-control-plaintext"
-            id="staticEmail2"
-            value={user.username}
-          />
-        </div>
+      <div className="profile-outer-div">
+        <form>
+          <div class="col-auto">
+            <label for="staticEmail2" class="visually-hidden">
+              Username:
+            </label>
+            <input
+              type="text"
+              readonly
+              class="form-control-plaintext"
+              id="staticEmail2"
+              value={user.username}
+            />
+          </div>
 
-        <div class="input-group mb-3">
-          <span class="input-group-text" id="inputGroup-sizing-default">
-            Name:
-          </span>
-          <input
-            type="text"
-            class="form-control"
-            aria-label="Sizing example input"
-            aria-describedby="inputGroup-sizing-default"
-            id="name"
-            name="name"
-            defaultValue={user.name}
-            onChange={handleChange}
-          />
-        </div>
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="inputGroup-sizing-default">
+              Name:
+            </span>
+            <input
+              type="text"
+              class="form-control"
+              aria-label="Sizing example input"
+              aria-describedby="inputGroup-sizing-default"
+              id="name"
+              name="name"
+              defaultValue={user.name}
+              onChange={handleChange}
+            />
+          </div>
 
-        <div class="input-group mb-3">
-          <span class="input-group-text" id="inputGroup-sizing-default">
-            Email:
-          </span>
-          <input
-            type="text"
-            class="form-control"
-            aria-label="Sizing example input"
-            aria-describedby="inputGroup-sizing-default"
-            id="email"
-            name="email"
-            defaultValue={user.email}
-            onChange={handleChange}
-          />
-        </div>
-        <div class="input-group mb-3">
-          <span class="input-group-text" id="inputGroup-sizing-default">
-            Street:
-          </span>
-          <input
-            class="form-control"
-            aria-label="Sizing example input"
-            aria-describedby="inputGroup-sizing-default"
-            type="text"
-            id="street"
-            name="street"
-            defaultValue={user.street}
-            onChange={handleChange}
-          />
-        </div>
-        <div class="input-group mb-3">
-          <span class="input-group-text" id="inputGroup-sizing-default">
-            Areacode:
-          </span>
-          <div>
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="inputGroup-sizing-default">
+              Email:
+            </span>
+            <input
+              type="text"
+              class="form-control"
+              aria-label="Sizing example input"
+              aria-describedby="inputGroup-sizing-default"
+              id="email"
+              name="email"
+              defaultValue={user.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="inputGroup-sizing-default">
+              Street:
+            </span>
             <input
               class="form-control"
               aria-label="Sizing example input"
               aria-describedby="inputGroup-sizing-default"
               type="text"
-              id="areaCode"
-              name="areaCode"
-              defaultValue={user.areacode}
+              id="street"
+              name="street"
+              defaultValue={user.street}
               onChange={handleChange}
             />
           </div>
-        </div>
-        <div class="input-group mb-3">
-          <span class="input-group-text" id="inputGroup-sizing-default">
-            City:
-          </span>
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="inputGroup-sizing-default">
+              Areacode:
+            </span>
+            <div>
+              <input
+                class="form-control"
+                aria-label="Sizing example input"
+                aria-describedby="inputGroup-sizing-default"
+                type="text"
+                id="areaCode"
+                name="areaCode"
+                defaultValue={user.areacode}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="inputGroup-sizing-default">
+              City:
+            </span>
 
-          <input
-            class="form-control"
-            aria-label="Sizing example input"
-            aria-describedby="inputGroup-sizing-default"
-            type="text"
-            id="city"
-            name="city"
-            defaultValue={user.city}
-            onChange={handleChange}
-          />
-        </div>
-        <button
-          className="submit-button-profile"
-          type="submit"
-          onClick={handleSubmit}
-          style={{ marginLeft: "1vh" }}
-        >
-          Update profile
-        </button>
-      </form>
-      <div className="container-profile"></div>
-      <form>
-        <div class="input-group mb-3">
-          <span class="input-group-text" id="inputGroup-sizing-default">
-            Current Password:
-          </span>
+            <input
+              class="form-control"
+              aria-label="Sizing example input"
+              aria-describedby="inputGroup-sizing-default"
+              type="text"
+              id="city"
+              name="city"
+              defaultValue={user.city}
+              onChange={handleChange}
+            />
+          </div>
+          <button
+            className="submit-button-profile"
+            type="submit"
+            onClick={handleSubmit}
+            style={{ marginLeft: "1vh" }}
+          >
+            Update profile
+          </button>
+        </form>
 
-          <input
-            class="form-control"
-            aria-label="Sizing example input"
-            aria-describedby="inputGroup-sizing-default"
-            type="text"
-            id="password"
-            name="password"
-            onChange={handleChangePassword}
-          />
-        </div>
-        <div class="input-group mb-3">
-          <span class="input-group-text" id="inputGroup-sizing-default">
-            New Password:
-          </span>
+        <form style={{ marginTop: "8vh" }}>
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="inputGroup-sizing-default">
+              Current Password:
+            </span>
 
-          <input
-            class="form-control"
-            aria-label="Sizing example input"
-            aria-describedby="inputGroup-sizing-default"
-            type="text"
-            id="newPassword"
-            name="newPassword"
-            onChange={handleChangePassword}
-          />
-        </div>
-        <div class="input-group mb-3">
-          <span class="input-group-text" id="inputGroup-sizing-default">
-            Repeat Password:
-          </span>
+            <input
+              class="form-control"
+              aria-label="Sizing example input"
+              aria-describedby="inputGroup-sizing-default"
+              type="text"
+              id="password"
+              name="password"
+              onChange={handleChangePassword}
+            />
+          </div>
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="inputGroup-sizing-default">
+              New Password:
+            </span>
 
-          <input
-            className="form-control"
-            aria-label="Sizing example input"
-            aria-describedby="inputGroup-sizing-default"
-            type="text"
-            id="repeatPassword"
-            name="repeatPassword"
-            onChange={handleChangePassword}
-          />
-        </div>
-        <button
-          className="submit-button-profile"
-          type="submit"
-          onClick={updatePassword}
-          style={{ marginLeft: "1vh" }}
-        >
-          Change password
-        </button>
-      </form>
+            <input
+              class="form-control"
+              aria-label="Sizing example input"
+              aria-describedby="inputGroup-sizing-default"
+              type="text"
+              id="newPassword"
+              name="newPassword"
+              onChange={handleChangePassword}
+            />
+          </div>
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="inputGroup-sizing-default">
+              Repeat Password:
+            </span>
+
+            <input
+              className="form-control"
+              aria-label="Sizing example input"
+              aria-describedby="inputGroup-sizing-default"
+              type="text"
+              id="repeatPassword"
+              name="repeatPassword"
+              onChange={handleChangePassword}
+            />
+          </div>
+          <button
+            className="submit-button-profile"
+            type="submit"
+            onClick={updatePassword}
+            style={{ marginLeft: "1vh" }}
+          >
+            Change password
+          </button>
+        </form>
+      </div>
     </>
   );
 };
