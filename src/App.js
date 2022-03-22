d270ff8ce314cbd2ec50ab8e2fd48788b7a7329d
Tajiana Odero <61429156+Taj-Ode@users.noreply.github.com>
2022-03-22 13:16:06 +0100

    Installed bootstrap, created Register.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import WelcomePageHeader from "./components/WelcomePageHeader";
 import WelcomePage from "./components/WelcomePage";
+import Register from "./components/Register";
 
 function App() {
   return (
     <main>
       <WelcomePageHeader />
       <WelcomePage />
+      <Register />
     </main>
   );
 };
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,7 +1,15 @@
+// import { Button, Form } from "react-bootstrap";
+
 const Register = () => {
     return (
-        <div>
+        <div className="register">
             <h4>Register</h4>
+            <input type="text" placeholder="First name"/>
+            <input type="text" placeholder="Last name"/>
+            <input type="text" placeholder="Username"/>
+            <input type="text" placeholder="Email"/>
+            <input type="text" placeholder="Password"/>
+            <button>Register</button>
         </div>
     );
 };
