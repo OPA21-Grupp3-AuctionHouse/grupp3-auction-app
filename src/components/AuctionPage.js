b1f4ee83c7dce947205653611dc51ed43f5a6f62
mollerpw <89844600+mollerpw@users.noreply.github.com>
2022-03-22 13:56:54 +0100

    updates to routing

--- src/components/AuctionPage.js
+++ src/components/AuctionPage.js
@@ -3,20 +3,39 @@ import React from "react";
 import AuctionHeader from "./AuctionHeader";
 import { BrowserRouter as Route, Routes } from "react-router-dom";
 import NewAuctionPage from "./NewAuctionPage";
+import Profile from "./Profile";
 const AuctionPage = (props) => {
   return (
     <>
       <AuctionHeader />
       <Routes>
-        <Route path="/auctions">
-          <NewAuctionPage />
-        </Route>
-        <Route path="/profile">
-          <NewAuctionPage />
-        </Route>
-        <Route path="/">
-          <NewAuctionPage />
-        </Route>
+        <Route
+          exact
+          path="/"
+          element={
+            <>
+                <NewAuctionPage />
+            </>
+          }
+        />
+        <Route
+          exact
+          path="/profile"
+          element={
+            <>
+                <Profile />
+            </>
+          }
+        />
+        <Route
+          exact
+          path="/auctions"
+          element={
+            <>
+                <NewAuctionPage />
+            </>
+          }
+        />
       </Routes>
     </>
   );
