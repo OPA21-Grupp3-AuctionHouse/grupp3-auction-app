8a925ed93ea94007196d76b1a0525034cb055c5f
mollerpw <89844600+mollerpw@users.noreply.github.com>
2022-03-24 09:29:37 +0100

    fix modal timeremaining

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,9 +1,35 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import ProductModal from "./ProductModal";
 
 const ProductCard = ({ product }) => {
   const [modalShow, setModalShow] = useState(false);
+  const [currentDate, setCurrentDate] = useState(new Date().getTime());
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentDate(new Date().getTime());
+    }, 1000);
 
+    return () => clearInterval(interval);
+  }, []);
+
+  const msToTime = (s) => {
+    const pad = (n, z) => {
+      z = z || 2;
+      return ("00" + n).slice(-z);
+    };
+    let ms = s % 1000;
+    s = (s - ms) / 1000;
+    let secs = s % 60;
+    s = (s - secs) / 60;
+    let mins = s % 60;
+    s = (s - mins) / 60;
+    let hrs = s % 24;
+    let days = (s - hrs) / 24;
+    return (
+      pad(days) + "D " + pad(hrs) + "H " + pad(mins) + "M " + pad(secs) + "S"
+    );
+  };
+  product.timeRemaining = msToTime(Date.parse(product.endTime) - currentDate);
   return (
     <>
       <div className="product-card" onClick={() => setModalShow(true)}>
