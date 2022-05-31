c8b99d9c5b8aa86f84971bc720bcba74b5984199
mollerpw <89844600+mollerpw@users.noreply.github.com>
2022-05-31 11:08:14 +0200

    browserroute test2

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename="/">
+    <BrowserRouter basename="/">
       <App />
-    </Router>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
